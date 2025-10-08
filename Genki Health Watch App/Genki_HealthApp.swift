6ca6b45b33c8a64a9a7ab8b116deb618a7744289
tuan.huynh <tuan.huynh@insentecs.com>
2025-10-08 21:24:34 +0700

    Initial Commit

---
+++ Genki Health Watch App/Genki_HealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Genki_HealthApp.swift
+//  Genki Health Watch App
+//
+//  Created by tuanhuynh on 8/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Genki_Health_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Genki Health Watch AppTests/Genki_Health_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Genki_Health_Watch_AppTests.swift
+//  Genki Health Watch AppTests
+//
+//  Created by tuanhuynh on 8/10/25.
+//
+
+import Testing
+@testable import Genki_Health_Watch_App
+
+struct Genki_Health_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
